e46bb54d7675a22a671323cd79e052acac7ae825
VaniaFirsov <firsov_vania@mail.ru>
2025-12-14 14:01:02 +0300

    ДЗ 1

---
+++ Module 1/HashMapCustom/src/HashMapImplementation.java
@@ -0,0 +1,130 @@
+public class HashMapImplementation<K,V> {
+    private Node<K,V>[] buckets;
+    private int size;
+    private int capacity;
+
+    private static class Node<K,V>{
+        K key;
+        V value;
+        Node<K,V> next;
+        Node(K key, V value){
+            this.key=key;
+            this.value=value;
+        }
+    }
+    HashMapImplementation(){
+        this.capacity=16;
+        this.size=0;
+        this.buckets=new Node[capacity];
+    }
+    //ищем значение по ключу
+    public V get(K key){
+        int keyIndex=0;
+        if(key==null){
+            //если key==null, то индекс это 0
+        }else{
+            keyIndex=Math.abs(key.hashCode())%capacity;
+        }
+        Node<K,V> keyNode=buckets[keyIndex];
+        while(keyNode!=null){
+            if(key==null){
+                if(keyNode.key==null){
+                    return keyNode.value;
+                }
+            }
+            else if(keyNode.key!=null && keyNode.key.equals(key)){
+                    return keyNode.value;
+            }
+            keyNode=keyNode.next;
+        }
+        return null;
+    }
+    //добавляем ноду, если уже есть такой ключ
+    //то меняем значение
+    public V put(K key, V value){
+        if(size/capacity>=0.75){
+            extendCapacity();
+        }
+        int keyIndex=0;
+        if(key==null){
+            //если key==null, то индекс это 0
+        }else{
+            keyIndex=Math.abs(key.hashCode())%capacity;
+        }
+        //проверяем, нет ли в бакете элемента с таким же ключом
+        Node<K,V> keyNode=buckets[keyIndex];
+        while(keyNode!=null){
+            if(key==null){
+                if(keyNode.key==null){
+                    V oldValue=keyNode.value;
+                    keyNode.value=value;
+                    return oldValue;
+                }
+            }
+            else if(keyNode.key!=null && keyNode.key.equals(key)){
+                //меняем значнеие
+                V oldValue=keyNode.value;
+                keyNode.value=value;
+                return oldValue;
+            }
+            keyNode=keyNode.next;
+        }
+        //добавляем новую ноду в бакет
+        Node<K,V> newNode=new Node<>(key,value);
+        newNode.next=buckets[keyIndex];
+        buckets[keyIndex]=newNode;
+        size++;
+        return null;
+    }
+    private void extendCapacity(){
+        int newCapacity=capacity*2;
+        Node<K,V>[] newBuckets=new Node[newCapacity];
+        for(int i=0;i<capacity;i++){
+            Node<K,V> current=buckets[i];
+            while(current!=null){
+                Node<K,V> next=current.next;
+                int newIndext=Math.abs(current.key.hashCode())%newCapacity;
+                current.next=newBuckets[newIndext];
+                newBuckets[newIndext]=current;
+                current=next;
+            }
+        }
+    }
+    //удалем ноду по ключу
+    public V remove(K key){
+        int keyIndex=0;
+        if(key==null){
+            //если key==null, то индекс это 0
+        }else{
+            keyIndex=Math.abs(key.hashCode())%capacity;
+        }
+        Node<K,V> current=buckets[keyIndex];
+        Node<K,V> previous=null;
+        while(current!=null){
+            if(key==null){
+                if(current.key==null){
+                    if(previous==null){
+                        buckets[keyIndex]=current.next;
+                    }else{
+                        previous.next=current.next;
+                    }
+                    size--;
+                    return current.value;
+                }
+            }
+
+            else if(current.key!=null && current.key.equals(key)){
+                if(previous==null){
+                    buckets[keyIndex]=current.next;
+                }else{
+                    previous.next=current.next;
+                }
+                size--;
+                return current.value;
+            }
+            previous=current;
+            current=current.next;
+        }
+        return null;
+    }
+}
---
+++ Module 1/HashMapCustom/src/Main.java
@@ -0,0 +1,27 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        HashMapImplementation<Integer,String> hash=new HashMapImplementation();
+        hash.put(1,"one");
+        hash.put(2,"two");
+        hash.put(3,"three");
+        hash.put(2,"twotwo");
+        hash.put(null,"nil");
+        hash.put(null,"nill");
+        hash.put(0,"zero");
+
+
+
+
+        System.out.println(hash.get(1));
+        System.out.println(hash.get(2));
+        System.out.println(hash.get(0));
+        System.out.println(hash.get(null));
+
+        System.out.println();
+        System.out.println(hash.get(3));
+        hash.remove(3);
+        System.out.println(hash.get(3));
+    }
+}
